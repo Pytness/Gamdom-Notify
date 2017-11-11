078b22af72d253ed9d82e5f058a73c5ed51f9b4b
Fernando Vega Catalán <pytness@gmail.com>
2017-11-11 17:20:03 +0100

    Created the payload loader

---
+++ gc_loader.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         GC Loader
+// @description  Load GC
+// @version      1.0
+// @author       Pytness
+// @match        *://gamdom.com/*
+// @run-at       document-start
+// @grant        GM_info
+// @grant        unsafeWindow
+// ==/UserScript==
+
+
+(function(w) {
+    'use strict';
+
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            eval(this.responseText);
+        }
+    };
+    ajax.open('GET', 'https://raw.githubusercontent.com/Pytness/Gamdom-Notify/master/tipper.js', true);
+    ajax.send(null);
+
+}(unsafeWindow));
