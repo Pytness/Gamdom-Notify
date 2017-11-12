09abad6191764117173d0beb0d00a33150195e58
Fernando Vega Catalán <pytness@gmail.com>
2017-11-12 20:15:06 +0100

    Improved the loader to pass an argument to the payoad

--- payload_loader.js
+++ payload_loader.js
@@ -16,7 +16,7 @@
     var ajax = new XMLHttpRequest();
     ajax.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            eval(this.responseText);
+            eval(this.responseText)(w);
         }
     };
     ajax.open('GET', 'https://raw.githubusercontent.com/Pytness/Gamdom-Notify/master/tipper.js', true);
